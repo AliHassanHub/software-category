3dca33da689ec3ad73c21d568fe428c6b72459c6
AliHassanHub <hassangill1206@gmail.com>
2025-04-25 21:10:08 -0700

    Initial commit: Add software category components with hover effects and text truncation

---
+++ src/pages/Index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SoftwareList } from "@/components/software/SoftwareList";
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+        rel="stylesheet"
+      />
+      <SoftwareList />
+    </>
+  );
+};
+
+export default Index;
